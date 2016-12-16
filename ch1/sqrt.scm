586662b558114c3edf26d414c7e1f6ab3d4caf7b
Akshat Harit <akshat.harit@gmail.com>
2016-12-15 22:14:39 -0800

    Modified sqrt to use block structure

--- ch1/sqrt.scm
+++ ch1/sqrt.scm
@@ -1,23 +1,24 @@
 #lang sicp
+
 (define (square x)
   (* x x))
+
 (define (abs x)
   (if (< x 0) (- x)
       x))
+
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
-
-(define (improve guess x)
-  (average guess (/ x guess)))
-
-(define (square-iter guess x)
-  (if (good-enough? guess x)
-      guess
-      (square-iter (improve guess x) x)))
-
 (define (sqrt x)
-  (square-iter 1.0 x))
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (square-iter guess)
+    (if (good-enough? guess)
+        guess
+        (square-iter (improve guess))))
+  (square-iter 1.0)
+)
 (sqrt 2)
